c61d33f588c007ccfafbb2a3275e4984a4f4b0f9
Noah Byers <noahxbyers@gmail.com>
2023-01-03 13:12:36 -1000

    Add: color class

---
+++ color.js
@@ -0,0 +1,12 @@
+// We'll need a string of 6 characters, and have to use 
+// math.random on an array of 0-9 and A-F
+// Potenial Issue: A-F will have to be a string, so we need to figure
+// out a way to combine strings with integers. 
+// REMEBER TO REFERENCE ARRAY WHEN YOU MOVE IT //
+
+class Color {
+  constructor() {
+    this.locked = false;
+    this.color ;
+  } 
+}
