129968ed27332bd479aa54ad7ba267d4a4ed6145
Michael Lobato <mike.lobato.wft@gmail.com>
2023-01-04 15:29:52 -0800

    Add: commented out function that needs revising

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //const Color = require("./color")
 //const Palette = require("./palette")
-
+ 
 //queryselectors
 var box1 =  document.querySelector("#box1")
 var box2 = document.querySelector("#box2")
@@ -16,12 +16,13 @@ var newPaletteButton = document.querySelector("#new-palette-button")
 
 //listeners
 window.addEventListener("load", showPalette)
-newPaletteButton.addEventListener("click", showPalette)
+newPaletteButton.addEventListener("click", replaceColor)
 
 //functions
 function showPalette() {
 createPalette()
 }
+
 
 function createPalette() {
     var color1 = new Color()
@@ -29,12 +30,7 @@ function createPalette() {
     var color3 = new Color()
     var color4 = new Color()
     var color5 = new Color()
-    color1.createColor()
-    color2.createColor()
-    color3.createColor()
-    color4.createColor()
-    color5.createColor()
-    var randomPalette = new Palette(color1, color2, color3, color4, color5) 
+    var randomPalette = new Palette(color1, color2, color3, color4, color5)
     box1.style.backgroundColor = randomPalette.colorPalette[0].color
     box2.style.backgroundColor = randomPalette.colorPalette[1].color
     box3.style.backgroundColor = randomPalette.colorPalette[2].color
@@ -46,6 +42,19 @@ function createPalette() {
     hexCode4.innerText = randomPalette.colorPalette[3].color
     hexCode5.innerText = randomPalette.colorPalette[4].color
 }
+// function replaceColor(randomPalette){
+//         randomPalette.replaceColors()
+//         box1.style.backgroundColor = randomPalette.colorPalette[0].color
+//         box2.style.backgroundColor = randomPalette.colorPalette[1].color
+//         box3.style.backgroundColor = randomPalette.colorPalette[2].color
+//         box4.style.backgroundColor = randomPalette.colorPalette[3].color
+//         box5.style.backgroundColor = randomPalette.colorPalette[4].color
+//         hexCode1.innerText = randomPalette.colorPalette[0].color
+//         hexCode2.innerText = randomPalette.colorPalette[1].color
+//         hexCode3.innerText = randomPalette.colorPalette[2].color
+//         hexCode4.innerText = randomPalette.colorPalette[3].color
+//         hexCode5.innerText = randomPalette.colorPalette[4].color
+//     }
 
 
 
