4f727b3c856453facbb284eafb92d79eaab06d40
Michael Lobato <mike.lobato.wft@gmail.com>
2023-01-04 13:55:59 -0800

    Add: assign random colors to each box upon page load

--- scripts.js
+++ scripts.js
@@ -13,20 +13,27 @@ window.addEventListener("load", showPalette)
 
 //functions
 function showPalette() {
-var color1 = new Color()
-var color2 = new Color()
-var color3 = new Color()
-var color4 = new Color()
-var color5 = new Color()
-color1.createColor()
-color2.createColor()
-color3.createColor()
-color4.createColor()
-color5.createColor()
+createPalette()
 
+}
 
-var randomPalette = new Palette(color1, color2, color3, color4, color5) 
-console.log(randomPalette)
+function createPalette() {
+    var color1 = new Color()
+    var color2 = new Color()
+    var color3 = new Color()
+    var color4 = new Color()
+    var color5 = new Color()
+    color1.createColor()
+    color2.createColor()
+    color3.createColor()
+    color4.createColor()
+    color5.createColor()
+    var randomPalette = new Palette(color1, color2, color3, color4, color5) 
+    box1.style.backgroundColor = randomPalette.colorPalette[0].color
+    box2.style.backgroundColor = randomPalette.colorPalette[1].color
+    box3.style.backgroundColor = randomPalette.colorPalette[2].color
+    box4.style.backgroundColor = randomPalette.colorPalette[3].color
+    box5.style.backgroundColor = randomPalette.colorPalette[4].color
 }
 
 
