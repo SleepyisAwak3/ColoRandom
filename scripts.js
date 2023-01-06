b625cf423534997f9b87e752470e3b3cddb4463a
Amber Shipley <ashipley2@me.com>
2023-01-06 08:56:35 -0800

    Refactor: variables, parameters, arguments, and inner html ids to start at 0 instead of 1

--- scripts.js
+++ scripts.js
@@ -1,34 +1,36 @@
+var color0 = new Color();
 var color1 = new Color();
 var color2 = new Color();
 var color3 = new Color();
 var color4 = new Color();
-var color5 = new Color();
-var randomPalette = new Palette(color1, color2, color3, color4, color5);
-var savedPalettes = []
+var randomPalette = new Palette(color0, color1, color2, color3, color4);
+var savedPalettes = [];
 
 //queryselectors
-var box1 =  document.querySelector("#box1");
+var box0 =  document.querySelector("#box0");
+var box1 = document.querySelector("#box1");
 var box2 = document.querySelector("#box2");
 var box3 = document.querySelector("#box3");
 var box4 = document.querySelector("#box4");
-var box5 = document.querySelector("#box5");
+var hexCode0 = document.querySelector("#hex0");
 var hexCode1 = document.querySelector("#hex1");
 var hexCode2 = document.querySelector("#hex2");
 var hexCode3 = document.querySelector("#hex3");
 var hexCode4 = document.querySelector("#hex4");
-var hexCode5 = document.querySelector("#hex5");
 var newPaletteButton = document.querySelector("#new-palette-button");
 var savePaletteButton = document.querySelector("#save-palette-button");
 var savedPaletteSection = document.querySelector(".saved-palettes");
-var savedMiniPalettes = document.querySelector(".saved-mini-palettes")
+var savedMiniPalettes = document.querySelector(".saved-mini-palettes");
+var miniBox0 = document.querySelector("#mini-box0");
 var miniBox1 = document.querySelector("#mini-box1");
 var miniBox2 = document.querySelector("#mini-box2");
 var miniBox3 = document.querySelector("#mini-box3");
 var miniBox4 = document.querySelector("#mini-box4");
-var miniBox5 = document.querySelector("#mini-box5");
 
 //listeners
-window.addEventListener("load", showPalette);
+window.addEventListener("load", function() {
+    showPalette(randomPalette)
+})
 newPaletteButton.addEventListener("click", function() {
     replaceColor(randomPalette)
 })
@@ -37,65 +39,65 @@ savePaletteButton.addEventListener("click", function() {
 })
 
 //functions
-function showPalette() {
+function showPalette(randomPalette) {
 createPalette(randomPalette);
 }
 
-function createPalette(randomPalette) {
-    box1.style.backgroundColor = randomPalette.colorPalette[0].color;
-    box2.style.backgroundColor = randomPalette.colorPalette[1].color;
-    box3.style.backgroundColor = randomPalette.colorPalette[2].color;
-    box4.style.backgroundColor = randomPalette.colorPalette[3].color;
-    box5.style.backgroundColor = randomPalette.colorPalette[4].color;
-    hexCode1.innerText = randomPalette.colorPalette[0].color;
-    hexCode2.innerText = randomPalette.colorPalette[1].color;
-    hexCode3.innerText = randomPalette.colorPalette[2].color;
-    hexCode4.innerText = randomPalette.colorPalette[3].color;
-    hexCode5.innerText = randomPalette.colorPalette[4].color;    
+function createPalette(palette) {
+    box0.style.backgroundColor = palette.colorPalette[0].color;
+    box1.style.backgroundColor = palette.colorPalette[1].color;
+    box2.style.backgroundColor = palette.colorPalette[2].color;
+    box3.style.backgroundColor = palette.colorPalette[3].color;
+    box4.style.backgroundColor = palette.colorPalette[4].color;
+    hexCode0.innerText = palette.colorPalette[0].color;
+    hexCode1.innerText = palette.colorPalette[1].color;
+    hexCode2.innerText = palette.colorPalette[2].color;
+    hexCode3.innerText = palette.colorPalette[3].color;
+    hexCode4.innerText = palette.colorPalette[4].color;    
 }
 
-function replaceColor(randomPalette) {
-    randomPalette.replaceColors();
-    box1.style.backgroundColor = randomPalette.colorPalette[0].color;
-    box2.style.backgroundColor = randomPalette.colorPalette[1].color;
-    box3.style.backgroundColor = randomPalette.colorPalette[2].color;
-    box4.style.backgroundColor = randomPalette.colorPalette[3].color;
-    box5.style.backgroundColor = randomPalette.colorPalette[4].color;
-    hexCode1.innerText = randomPalette.colorPalette[0].color;
-    hexCode2.innerText = randomPalette.colorPalette[1].color;
-    hexCode3.innerText = randomPalette.colorPalette[2].color;
-    hexCode4.innerText = randomPalette.colorPalette[3].color;
-    hexCode5.innerText = randomPalette.colorPalette[4].color;
+function replaceColor(palette) {
+    palette.replaceColors();
+    box0.style.backgroundColor = palette.colorPalette[0].color;
+    box1.style.backgroundColor = palette.colorPalette[1].color;
+    box2.style.backgroundColor = palette.colorPalette[2].color;
+    box3.style.backgroundColor = palette.colorPalette[3].color;
+    box4.style.backgroundColor = palette.colorPalette[4].color;
+    hexCode0.innerText = palette.colorPalette[0].color;
+    hexCode1.innerText = palette.colorPalette[1].color;
+    hexCode2.innerText = palette.colorPalette[2].color;
+    hexCode3.innerText = palette.colorPalette[3].color;
+    hexCode4.innerText = palette.colorPalette[4].color;
 }
 
-function savePalette(randomPalette, savedPalettes) {
-    if (!savedPalettes.includes(randomPalette)) {
-        savedPalettes.push(randomPalette)
+function savePalette(palette, paletteArray) {
+    if (!paletteArray.includes(palette)) {
+        paletteArray.push(palette)
     }
-  displaySavedPalettes(savedPalettes);
+  displaySavedPalettes(paletteArray);
 }
 
-function displaySavedPalettes(savedPalettes) {
+function displaySavedPalettes(paletteArray) {
     savedMiniPalettes.innerHTML = ""
-    for (var i = 0; i < savedPalettes.length; i++) {
+    for (var i = 0; i < paletteArray.length; i++) {
         savedMiniPalettes.innerHTML += `
          <section class="saved-mini-palette">
-              <div class="box mini" id="mini-box1" style= "background-color: ${savedPalettes[i].colorPalette[0].color};"></div>
-              <div class="box mini" id="mini-box2" style= "background-color: ${savedPalettes[i].colorPalette[1].color};"></div>
-              <div class="box mini" id="mini-box3" style= "background-color: ${savedPalettes[i].colorPalette[2].color};"></div>
-              <div class="box mini" id="mini-box4" style= "background-color: ${savedPalettes[i].colorPalette[3].color};"></div>
-              <div class="box mini" id="mini-box5" style= "background-color: ${savedPalettes[i].colorPalette[4].color};"></div>
-              <p class="trash" data-delete-id="${savedPalettes[i].id}"> 🗑 </p>
+              <div class="box mini" id="mini-box0" style= "background-color: ${paletteArray[i].colorPalette[0].color};"></div>
+              <div class="box mini" id="mini-box1" style= "background-color: ${paletteArray[i].colorPalette[1].color};"></div>
+              <div class="box mini" id="mini-box2" style= "background-color: ${paletteArray[i].colorPalette[2].color};"></div>
+              <div class="box mini" id="mini-box3" style= "background-color: ${paletteArray[i].colorPalette[3].color};"></div>
+              <div class="box mini" id="mini-box4" style= "background-color: ${paletteArray[i].colorPalette[4].color};"></div>
+              <p class="trash" data-delete-id="${paletteArray[i].id}"> 🗑 </p>
         </section>
        `;        
     }
+    color0 = new Color();
     color1 = new Color();
     color2 = new Color();
     color3 = new Color();
     color4 = new Color();
-    color5 = new Color();
-    randomPalette = new Palette(color1, color2, color3, color4, color5);
-    showPalette();
+    randomPalette = new Palette(color0, color1, color2, color3, color4);
+    showPalette(randomPalette);
 }
 
 //=========NOTES=========//
