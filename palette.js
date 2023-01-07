2d347dee8e7afc6e41d095b91a5dbe6cde83d2c5
Amber Shipley <ashipley2@me.com>
2023-01-06 19:31:10 -0800

    Change: name of lockColor function to toggleLock so that it is reusable and more dynamic, and create functionality

--- palette.js
+++ palette.js
@@ -1,27 +1,22 @@
 class Palette {
-  constructor() {
-    var color0 = new Color();
-    var color1 = new Color();
-    var color2 = new Color();
-    var color3 = new Color();
-    var color4 = new Color();
-    this.colorPalette = [color0, color1, color2, color3, color4];
-    this.id = Date.now();
-  }
-  replaceColors() {
-    for (var i = 0; i < this.colorPalette.length; i++) {
-      if (!this.colorPalette[i].locked) {
-        this.colorPalette[i].createColor();
-      }
-    }
-  }
-  lockColor() {
-    console.log("before")
-    for (var i = 0; i < this.colorPalette.length; i++) {
-      if (event.target.id)
-      this.colorPalette[0].locked = true; 
-      console.log("after")
-    }
-  }
+	constructor() {
+		var color0 = new Color();
+		var color1 = new Color();
+		var color2 = new Color();
+		var color3 = new Color();
+		var color4 = new Color();
+		this.colorPalette = [color0, color1, color2, color3, color4];
+		this.id = Date.now();
+	}
+	replaceColors() {
+		for (var i = 0; i < this.colorPalette.length; i++) {
+			if (!this.colorPalette[i].locked) {
+				this.colorPalette[i].createColor();
+			}
+		}
+	}
+	toggleLock(colorIndex) {
+		this.colorPalette[colorIndex].locked = !this.colorPalette[colorIndex].locked;
+		console.log(this.colorPalette);
+	}
 }
-// lockColor this.colorPalette[0] needs to be more dynamic after 
